25f25dcb40397f5588b41af1aacb9ebfc1fd7908
Euismutnh <euisrohimattul@gmail.com>
2023-08-20 23:07:38 +0700

    Pengumpulan Tugas

--- 3_Fundamental Dart/main.dart
+++ 3_Fundamental Dart/main.dart
@@ -1,17 +1,29 @@
 import 'dart:io';
+import 'persegi.dart';
+import 'persegi panjang.dart';
 
 void main() {
   print('Selamat datang di bahasa pemrogramman dart');
+  print(
+      'Mari kita menghitung Luas dan keliling dari persegi dan persegi panjang');
+  stdout.write("Masukkan panjang sisi persegi: ");
+  double sisiPersegi = double.parse(stdin.readLineSync()!);
 
-  var a;
-  a = 1;
-  print(a);
+  Persegi persegi = Persegi(sisiPersegi);
+  print("Persegi:");
+  print("Luas: ${persegi.hitungLuas()}");
+  print("Keliling: ${persegi.hitungKeliling()}");
 
-  var b = 2;
-  print(b);
+  stdout.write("\nMasukkan panjang dan lebar persegi panjang : ");
+  List<String> inputPersegiPanjang = stdin.readLineSync()!.split(" ");
+  double panjangPersegiPanjang = double.parse(inputPersegiPanjang[0]);
+  double lebarPersegiPanjang = double.parse(inputPersegiPanjang[1]);
 
-  //ini cara agar user bisa menginputkan hasil yang di inginkan dengan menggunakan fungsi int.parse(stdin.readLineSync()!);
-  print("Tuliskan angka favorite kamu : ");
-  int n = int.parse(stdin.readLineSync()!);
-  print("");
+  PersegiPanjang persegiPanjang =
+      PersegiPanjang(panjangPersegiPanjang, lebarPersegiPanjang);
+  print("\nPersegi Panjang:");
+  print("Luas: ${persegiPanjang.hitungLuas()}");
+  print("Keliling: ${persegiPanjang.hitungKeliling()}");
+
+  // Ini cara agar user bisa menginputkan hasil yang diinginkan dengan menggunakan fungsi int.parse(stdin.readLineSync()!);
 }
---
+++ 3_Fundamental Dart/persegi.dart
@@ -0,0 +1,13 @@
+class Persegi {
+  double sisi;
+
+  Persegi(this.sisi);
+
+  double hitungLuas() {
+    return sisi * sisi;
+  }
+
+  double hitungKeliling() {
+    return 4 * sisi;
+  }
+}
